a313836822df3b1f70a91e1b42e6c1459435a3e0
Christopher Davies-Jenkins <c.w.daviesjenkins@gmail.com>
2023-02-03 16:13:35 -0500

    Update CWDJ_SavFig.m
    WIP: added modifiable size to the PDF output

--- IO/CWDJ_SavFig.m
+++ IO/CWDJ_SavFig.m
@@ -1,17 +1,16 @@
-function [] = CWDJ_SavFig( FigHandle , SavLoc)
-%% function [] = CWDJ_SavFig( FigHandle , SavLoc)
-% 
-% Function for saving a Matlab figure as .fig and .png at specified location
-%
-% Optional: FigHandle = Handle of target figure (default=gcf)
-%           SavLoc = Path to save figure and pdf with no ext (default=pwd)
+function [] = CWDJ_SavFig( FigHandle , SavLoc, Size)
+%Save a figure as .fig and .png at specified location
+% Opt:      FigHandle - Figure number of target figure (default=gcf)
+%           SavLoc - Path to save figure and pdf with no ext (default=pwd)
+%           Size - Size (in inches) of figure units.
 %
 % Example usage:
-% CWDJ_SavFig(1, 'Path/To/Output/Location')
+%   CWDJ_DavFig(1, /Path/To/Figure, [20, 20])
+%
 %
 % C.W.Davies-Jenkins, Johns Hopkins 2022
 
-% Handle missing variables
+% Handle some missing optional variables:
 if exist("FigHandle","var")
     FIG=figure(FigHandle);
 else
@@ -21,14 +20,41 @@ if ~exist('SavLoc','var')
     SavLoc = fullfile(pwd,'CWDJ_Fig');
 end
 
-Title_PDF = [SavLoc,'.pdf'];
-Title_FIG = [SavLoc,'.fig'];
+% Create directory if it doesn't exist:
+Dir = fileparts(SavLoc);
+if ~exist(Dir,'dir')
+    mkdir(Dir);
+end
 
-set(FIG,'Units','Inches');
-pos = get(FIG,'Position');
-set(FIG,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+set(FIG,'Units','Inches'); % Fix figure units to inches
+pos = get(FIG,'Position'); % Get current position of figure
 
-print(FIG,Title_PDF,'-dpdf','-vector')
-savefig(FIG,Title_FIG,'compact')
+% If figure size not specified, take current dimensions
+if ~exist("Size",'var')
+    Size = pos(3:4);
+end
+
+set(FIG,'PaperSize',Size) % Fix figure paper size as specified
+
+FormatFig(FIG) % some additional formatting for sub figures (seee below)
+
+print(FIG,[SavLoc,'.pdf'],'-dpdf','-vector') % Print PDF vector format
+savefig(FIG,[SavLoc,'.fig'],'compact') % Save figure as .fig
+
+end
+
+
+function[] = FormatFig(FIG)
+
+AllAxes = findall(FIG,'type','axes');
+
+% For each axis:
+%   1) Move ticks outside lines
+%   2) Remove outer box
+
+for JJ=1:length(AllAxes)
+    set(AllAxes(JJ),'TickDir','out','Box','off');
+end
+
 
 end
