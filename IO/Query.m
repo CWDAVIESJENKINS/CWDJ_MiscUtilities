ebded037c62c63ec5a83cdb8c8362e7a0a06de7d
Christopher Davies-Jenkins <c.w.daviesjenkins@gmail.com>
2023-08-02 15:18:25 -0400

    Add "NOT" searching to Query function
    Values preceded by "~" will now trigger a NOT search: removing all files that contain that entry, rather than retaining them.

--- IO/Query.m
+++ IO/Query.m
@@ -5,8 +5,10 @@ function[FilesOut, ListStruct] = Query(ListStruct,varargin)
 %
 % Input:    ListStruct = Struct output by FindFiles.m
 %           varargin = Arbitrary-length set of query pairs i.e. the "key" to
-%           be queried, followed by the desired "value". Can also supply a
-%           ListStruct as value, and match entries (e.g. Participant I.D.).
+%           be queried, followed by the desired "value". Preceding the 
+%           value by "~" will search for entries NOT containing that entry 
+%           (exclusions) Can also supply an external ListStruct as value, 
+%           and match entries (e.g. Participant I.D.).      
 % Output:   FilesOut = cell array containing full paths to querried files
 %
 % Example usage:
@@ -37,7 +39,13 @@ for JJ=1:length(varargin)/2
         [~,Keep] = intersect(Entries, {Value.(Key)}); % Find all files 
     else
         Entries(cellfun(@isnumeric, Entries)) = ''; % Replace empty entries with blank cell
-        Keep = contains(Entries, Value); % Find entries matching query
+        if ischar(Value) && strcmp(Value(1),'~') % If the search string is preceeded by "~" search for enties that don't have "Value"
+            Value = Value(2:end); % Remove "~"
+            Keep = ~contains(Entries, Value); % Find entries NOT matching query
+        else
+            Keep = contains(Entries, Value); % Find entries matching query
+        end
+        
     end
     ListStruct = ListStruct(Keep); % Keep only files that match the string search
 end
