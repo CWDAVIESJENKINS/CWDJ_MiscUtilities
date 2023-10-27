f7d0ec4a3cb32420d91c398d3eb905dc94d82eb4
Christopher Davies-Jenkins <c.w.daviesjenkins@gmail.com>
2023-10-27 12:14:52 -0400

    Update CWDJ_SavFig.m
    The function now exports with a transparent background by default.

--- IO/CWDJ_SavFig.m
+++ IO/CWDJ_SavFig.m
@@ -1,16 +1,17 @@
 function [] = CWDJ_SavFig( FigHandle , SavLoc, Size)
-%Save a figure as .fig and .png at specified location
+%Save a figure as .fig and .pdf at specified location
 % Opt:      FigHandle - Figure number of target figure (default=gcf)
 %           SavLoc - Path to save figure and pdf with no ext (default=pwd)
-%           Size - Size (in inches) of figure units.
+%           Size - Size (in inches) of figure units (default=preserves 
+%                  current aspect ratio)
 %
 % Example usage:
-%   CWDJ_DavFig(1, /Path/To/Figure, [20, 20])
-%
+%   CWDJ_SavFig(1, /Path/To/Figure, [3, 4]) % Saves Fig.1 at path, with 
+%   size of 3x4 inches
 %
 % C.W.Davies-Jenkins, Johns Hopkins 2022
 
-% Handle some missing optional variables:
+%% Handle some missing optional variables:
 if exist("FigHandle","var")
     FIG=figure(FigHandle);
 else
@@ -20,12 +21,13 @@ if ~exist('SavLoc','var')
     SavLoc = fullfile(pwd,'CWDJ_Fig');
 end
 
-% Create directory if it doesn't exist:
+%% Create directory if it doesn't exist:
 Dir = fileparts(SavLoc);
 if ~exist(Dir,'dir')
     mkdir(Dir);
 end
 
+%% Manage size and positioning
 set(FIG,'Units','Inches'); % Fix figure units to inches
 pos = get(FIG,'Position'); % Get current position of figure
 
@@ -36,9 +38,12 @@ end
 
 set(FIG,'PaperSize',Size) % Fix figure paper size as specified
 
+
 FormatFig(FIG) % some additional formatting for sub figures (seee below)
 
-print(FIG,[SavLoc,'.pdf'],'-dpdf','-vector') % Print PDF vector format
+%% Save 
+
+exportgraphics(FIG,[SavLoc,'.pdf'],'ContentType','vector','BackgroundColor','none') % Print PDF vector format
 savefig(FIG,[SavLoc,'.fig'],'compact') % Save figure as .fig
 
 end
@@ -51,10 +56,11 @@ AllAxes = findall(FIG,'type','axes');
 % For each axis:
 %   1) Move ticks outside lines
 %   2) Remove outer box
+%   3) Set background color to none
 
 for JJ=1:length(AllAxes)
-    set(AllAxes(JJ),'TickDir','out','Box','off');
+    set(AllAxes(JJ),'TickDir','out','Box','off','color','none');
 end
-
+set(FIG, 'color', 'none');
 
 end
