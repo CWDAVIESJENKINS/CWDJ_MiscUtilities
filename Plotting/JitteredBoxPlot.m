5f1390d5f3513979a689fe8316c63ceb3db35d9e
Christopher Davies-Jenkins <c.w.daviesjenkins@gmail.com>
2024-03-12 13:51:57 -0400

    Update JitteredBoxPlot.m
    Modified JitteredBoxPlot to work with cell arrays as well as matrices, and added additional visualization options.

--- Plotting/JitteredBoxPlot.m
+++ Plotting/JitteredBoxPlot.m
@@ -1,40 +1,89 @@
-function[BC] = JitteredBoxPlot(InMat, Colors)
+function[BC] = JitteredBoxPlot(InMat, Colors, varargin)
 %% function[BC] = JitteredBoxPlot(InMat, Colors)
 %
 % Description: Plot boxplots alongside scattered individual points.
 %
 % Input:     InMat = Matrix with columns representing seperate measures
 % Optional:  Colors = RGB color vectors, with rows corresponding to the
-%            columns of InMat (default uses cbrewer)
+%                     columns of InMat (default uses cbrewer)
+%            varargin = Additional (pairwise options)
 % Output:    BC = Boxchar array
 %
 % Example usage: JitterBoxPlot(DataMatrix, ColorMatrix);
 %
+% Additional varargin:
+%   Connection = Nx2 vector. Plots links between points specified in the 
+%                1st & 2nd colums of each row. Default: []
+%   Width = Width of the scatter distribution. Default: 0.2;
+%   Offset = How shifted the scatter is, relative to box. Default: 0.4
+%   PlotOutliers = Whether to vizualize outliers inline with boxplot.
+%                  Default: false.
+%
 % C.W. Davies-Jenkins, Johns Hopkins University 2023
 
-%% Manage inputs
-S = size(InMat);
-if ~exist('Colors','var') || isempty(Colors)
-    Colors = cbrewer('qual','Dark2',S(1)); % Use cbrewer function to generate RGB vectors using the "Dark2" set
-else
-%     ColSize = size(Colors);
-%     if ColSize(1)<S(1)
-%         error('Color vector is too small (%i, compared to %i matrix columsn)',ColSize,S(1));
-%     end
+%% Check inputs
+
+% Convert to cell array (if matrix or table)
+if ~iscell(InMat) && ~istable(InMat)
+    S = size(InMat);
+    M=InMat;InMat = cell(1,S(2));
+    for JJ=1:S(2)
+        InMat{JJ} = M(:,JJ);
+    end
+elseif istable(InMat)
+    keyboard
 end
 
-%% Define hardcoded paramters
+% Create/check color matrix
+if ~exist('Colors','var') || isempty(Colors)
+    Colors = cbrewer('qual','Dark2',length(InMat)); % Use cbrewer function to generate RGB vectors using the "Dark2" set
+else
+    ColSize = size(Colors);
+    if ColSize(1)<length(InMat)
+        error('Color vector is too small (%i, compared to %i matrix columns)',ColSize,S(1));
+    end
+end
 
+%% Manage Varargin
+
+%%%%% Inititalize default parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Connection = [];
 Width = 0.2; % Width of the scatter
 Offset = 0.4; % How shifted the scatter is (set to 0 to plot over)
+PlotOutliers = false;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Overwrite default parameters, if specified in varargin
+if nargin>2
+    L = length(varargin)/2;
+    Args = reshape(varargin,2,L)';
+    for JJ=1:L
+        switch Args{JJ,1}
+            case "Connection"
+                Connection = Args{JJ,2};
+            case "Width"
+                Width = Args{JJ,2};
+            case "Offset"
+                Offset = Args{JJ,2};
+            case "PlotOutliers"
+                PlotOutliers = Args{JJ,2};
+            otherwise
+                warning('Unknown option: %s',Args{JJ,2})
+        end
+    end
+end
 
 %% Loop and plot
 
-for JJ=1:S(2)
-    BC(JJ) = boxchart(JJ*ones(1,S(1)),InMat(:,JJ),'BoxFaceColor',Colors(JJ,:),'MarkerColor',Colors(JJ,:));
+for JJ=1:length(InMat)
+    if PlotOutliers
+        BC(JJ) = boxchart(JJ*ones(1,length(InMat{JJ})),InMat{JJ},'BoxFaceColor',Colors(JJ,:),'MarkerColor',Colors(JJ,:));
+    else
+        BC(JJ) = boxchart(JJ*ones(1,length(InMat{JJ})),InMat{JJ},'BoxFaceColor',Colors(JJ,:),'BoxFaceColor',Colors(JJ,:),'MarkerStyle','none');
+    end
     hold on
-    Jitter = rand([1,S(1)]).*Width - Width/2 + JJ-Offset;
-    plot(Jitter, InMat(:,JJ),'.',"Color",Colors(JJ,:),'MarkerFaceColor',Colors(JJ,:),'MarkerSize',10)
+    Jitter{JJ} = rand([1,length(InMat{JJ})]).*Width - Width/2 + JJ-Offset;
+    plot(Jitter{JJ}, InMat{JJ},'.',"Color",Colors(JJ,:),'MarkerFaceColor',Colors(JJ,:),'MarkerSize',10)
 end
 
 xlim([1-2*Offset, JJ+Offset])
@@ -42,4 +91,18 @@ xlim([1-2*Offset, JJ+Offset])
 Ax = gca;
 Ax.XAxis.Visible = 'off'; % remove x-axis
 
+%% Plot connected points
+
+if ~isempty(Connection)
+    Sc = size(Connection);
+    for JJ=1:Sc(1)
+        L1 = length(Jitter{Connection(JJ,1)});
+        L2 = length(Jitter{Connection(JJ,2)});
+        if ~(L1 == L2)
+            error("Can't plot this connection! Entry %i has different lengths: %i and %i",JJ,L1,L2);
+        end
+        for KK=1:L1
+            plot([Jitter{Connection(JJ,1)}(KK), Jitter{Connection(JJ,2)}(KK)],[InMat{Connection(JJ,1)}(KK), InMat{Connection(JJ,2)}(KK)],'Color',[0,0,0,0.4],'LineWidth',0.2)
+        end
+    end
 end
