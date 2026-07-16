b250861f769aa5b1fc4f9859e3f9eff43c983089
Christopher Davies-Jenkins <c.w.daviesjenkins@gmail.com>
2026-07-16 12:48:38 -0400

    Add highlighted UI table to DataCompleteness.m
    Now generates a table that highlights datasets with fewer entries than expected.

--- BIDS/DataCompleteness.m
+++ BIDS/DataCompleteness.m
@@ -1,10 +1,14 @@
-function[Out_Table] = DataCompleteness(FileStruct)
-%% function[Out_Table] = DataCompleteness(FileStruct)
+function[Out_Table] = DataCompleteness(FileStruct, Visualize)
+%% function[Out_Table] = DataCompleteness(FileStruct, Visualize)
 %
 % Description: A function to generate a report of the completeness of a
-% BIDS-compliant dataset. Makes use of the FindFiles data struct.
+% BIDS-compliant dataset. Makes use of the FindFiles data struct. It
+% removes subject-level info from filenames and compares unique entries
+% across subjects.
 %
 % Input:     FileStruct = Matlab struct output by the FindFiles function
+% Optional:  Visualize = Bool whether to open UI table with highlights
+%               (default = true)
 % Output:    Out_Table = A table summarizing the found files (by subject)
 %
 % Example usage:
@@ -13,6 +17,9 @@ function[Out_Table] = DataCompleteness(FileStruct)
 %
 % C.W. Davies-Jenkins, Johns Hopkins University 2024
 
+if ~exist('Visualize','var')
+    Visualize = true;
+end
 
 %% Establish the primary fields (subjects & sessions)
 
@@ -62,4 +69,20 @@ Out_Table(end+1,:)=[{'Total:'},TotalNumber];
 
 
 
+%% If enabled, visualize the output in a UI table that highlights missing data
+if Visualize
+    % Establish cells to highlight
+    ValBool = Out_Table(1:end-1,2:end)==0; % If a particular file type is not present, highlight
+    N = length(Out_Table.Subject)-1;
+    TotBool = Out_Table(end,2:end)<N;      % :If the total is less than N, highlight
+    [Rows,Cols] = find(table2array([ValBool;TotBool]));
+    Cols = Cols+1; % (Exclude first colum (Subject ID)
+    
+    % Create UI table
+    Fig = uifigure('Name', 'Data completeness review', 'Position', [100 100 1600 950]);
+    uitbl = uitable(Fig, 'Data', Out_Table, 'Position', [20 20 1560 900]);
+
+    % Apply the highlight style
+    warningStyle = uistyle('BackgroundColor',[246, 255, 0]./255, 'FontWeight', 'bold');
+    addStyle(uitbl, warningStyle, 'cell', [Rows, Cols]);
 end
